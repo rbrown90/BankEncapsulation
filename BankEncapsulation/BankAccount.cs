cc2789ed022fecd3c8435f25dac819bb13a62be2
Rakin Brown <rakinbrown38@gmail.com>
2023-04-26 18:00:19 -0500

    bank modifier update

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+
+        public BankAccount()
+        {
+        }
+
+		private double balance = 0;
+
+        public void Deposit(double ammount)
+		{
+			balance = ammount;
+		}
+
+		public double GetBalance()
+		{
+			return balance;
+		}
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace BankEncapsulation
+﻿using System.Collections.Generic;
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to Deposit?");
+
+            var depositAmmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(depositAmmount);
+
+            Console.WriteLine($"You have deposited {depositAmmount} today. Your new balanace now is {account.GetBalance()} Thank you!");
         }
     }
 }
